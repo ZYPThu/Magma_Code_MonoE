278a1fbda3fa8576fbbd9e3576ee1ddcef1f87dd
ZYPThu <53860799+ZYPThu@users.noreply.github.com>
2022-12-01 16:28:41 +0800

    Update Winger.m

--- Winger.m
+++ Winger.m
@@ -1,3 +1,5 @@
+/*This is the Magma Code for the article Monodromy and Period Map of the Winger Pencil II: $E$-part./*
+/*You could copy it to the following site http://magma.maths.usyd.edu.au/calc/ and get the result./*
 /* Presentation for SL(2,O) */
 G<z, s, m, t, e> := Group<z, s, m, t, e | z^2, (z,s), (z,m), (z,t), (z,e), s^2*z, (s*t)^3, (s*m)^2*z, (t, e), m*t*m^-1=t*e, m*e*m^-1=t*e^2, s*e*s=t*e^-1*s*e^-1*m*z>; 
 
